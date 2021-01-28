38bfc9a17043391ca32ba205d2b776a433a4c628
techinee <6206021621066@fitm.kmutnb.ac.th>
2021-01-28 08:39:35 +0700

    create

--- cloneObj.js
+++ cloneObj.js
@@ -2,13 +2,13 @@
 //Obj Ref,Obj clonning assign(),{...x},JSON
 //
 const obj1 = {
-    person : 'Puchcharapoom',
-    Weight : 85
+    person : 'Techinee',
+    Weight : 43
 }
 
 //const obj2 = obj1;
 const obj2 = Object.assign({},obj1);
 //const obj2 = {...obj1};
-obj2.Weight=73;
+obj2.Weight=45;
 console.log(obj1);
 console.log(obj2);
